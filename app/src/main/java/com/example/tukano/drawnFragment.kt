9eb29044554b8ce4021fb52847cb7d971f3ac61e
lobatolc <lucaslobto@hotmail.com>
2022-04-06 14:46:20 -0300

    creation of dialog boxes

--- app/src/main/java/com/example/tukano/drawnFragment.kt
+++ app/src/main/java/com/example/tukano/drawnFragment.kt
@@ -1,23 +1,16 @@
 package com.example.tukano
 
-import android.R
 import android.content.DialogInterface
-import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.SeekBar
-import android.widget.Spinner
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import com.example.tukano.databinding.ColorDialogUI
 import com.example.tukano.databinding.DrawnUI
+import com.example.tukano.databinding.SettingsDialogUI
 import com.github.gcacace.signaturepad.views.SignaturePad
-import com.google.android.material.snackbar.Snackbar
-import com.madrapps.pikolo.ColorPicker
 import com.madrapps.pikolo.listeners.SimpleColorSelectionListener
 
 
@@ -30,6 +23,28 @@ class drawnFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DrawnUI.inflate(inflater)
+        val bindingColor = ColorDialogUI.inflate(inflater)
+
+        val builder = android.app.AlertDialog.Builder(requireContext())
+
+        builder
+            .setTitle("Selecione a cor desejada")
+            .setPositiveButton("Confirmar", DialogInterface.OnClickListener { dialog, which ->
+
+            })
+            .setView(bindingColor.root)
+        val dialog = builder.create()
+
+        val bindingConfig = SettingsDialogUI.inflate(inflater)
+
+        val builderConfig = android.app.AlertDialog.Builder(requireContext())
+        builderConfig
+            .setTitle("Configurações do Pincel")
+            .setPositiveButton("Confirmar", DialogInterface.OnClickListener { dialog, which ->
+
+            })
+            .setView(bindingConfig.root)
+        val dialogConfig = builderConfig.create()
 
         binding.signaturePad.setOnSignedListener(object : SignaturePad.OnSignedListener {
             override fun onStartSigning() {
@@ -55,37 +70,33 @@ class drawnFragment : Fragment() {
 
 
         binding.btnChangeColor.setOnClickListener {
-            binding.btnChangeConfirm.visibility = View.VISIBLE
-            binding.colChangeColor.visibility = View.VISIBLE
 
-            binding.colChangeColor.setColorSelectionListener(object : SimpleColorSelectionListener() {
+            dialog.show()
+
+            bindingColor.colChangeColor.setColorSelectionListener(object : SimpleColorSelectionListener() {
                 override fun onColorSelected(color: Int) {
                     // Do whatever you want with the color
                     binding.signaturePad.setPenColor(color)
-                    binding.btnChangeConfirm.colorNormal = color
-
-                    binding.btnChangeConfirm.colorPressed = color
+                    bindingColor.btnChangeConfirm.colorNormal = color
+                    bindingColor.btnChangeConfirm.colorPressed = color
                 }
             })
-        }
+            }
+
+
+
 
         binding.btnSettings.setOnClickListener {
-            binding.sekPencil.visibility = View.VISIBLE
-            binding.btnSeek.visibility = View.VISIBLE
-            binding.sekVelocity.visibility = View.VISIBLE
+            dialogConfig.show()
         }
 
-        binding.btnSeek.setOnClickListener {
-            binding.sekPencil.visibility = View.GONE
-            binding.btnSeek.visibility = View.GONE
-            binding.sekVelocity.visibility = View.GONE
 
-        }
 
-        binding.sekPencil.setOnSeekBarChangeListener(object :
+        bindingConfig.sekPencil.setOnSeekBarChangeListener(object :
             SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 binding.signaturePad.setMinWidth(progress-2f)
+                bindingConfig.txtSize.text = "Tamanho: $progress"
                 binding.signaturePad.setMaxWidth(progress+2f)
             }
 
@@ -96,12 +107,12 @@ class drawnFragment : Fragment() {
             }
         })
 
-        binding.sekVelocity.setOnSeekBarChangeListener(object :
+        bindingConfig.sekVelocity.setOnSeekBarChangeListener(object :
             SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 var press = progress/100
                 binding.signaturePad.setVelocityFilterWeight(press.toFloat())
-
+                bindingConfig.txtVelocity.text = "Velocidade: $progress"
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -111,11 +122,10 @@ class drawnFragment : Fragment() {
             }
         })
 
-        binding.btnChangeConfirm.setOnClickListener {
 
-            binding.btnChangeConfirm.visibility = View.GONE
-            binding.colChangeColor.visibility = View.GONE
-        }
+
+
+
         return binding.root
     }
 
