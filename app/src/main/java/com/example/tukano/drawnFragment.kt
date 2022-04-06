223b140a75510ee394da5e0f2e288610075d7fb3
lobatolc <lucaslobto@hotmail.com>
2022-04-06 12:28:29 -0300

    drawning and color picker tool

--- app/src/main/java/com/example/tukano/drawnFragment.kt
+++ app/src/main/java/com/example/tukano/drawnFragment.kt
@@ -1,59 +1,81 @@
 package com.example.tukano
 
+import android.R
+import android.content.DialogInterface
+import android.graphics.PorterDuff
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.tukano.databinding.DrawnUI
+import com.github.gcacace.signaturepad.views.SignaturePad
+import com.google.android.material.snackbar.Snackbar
+import com.madrapps.pikolo.ColorPicker
+import com.madrapps.pikolo.listeners.SimpleColorSelectionListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [drawnFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+private lateinit var binding: DrawnUI
 class drawnFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_drawn, container, false)
+        binding = DrawnUI.inflate(inflater)
+
+        binding.signaturePad.setOnSignedListener(object : SignaturePad.OnSignedListener {
+            override fun onStartSigning() {
+                //Event triggered when the pad is touched
+
+            }
+
+            override fun onSigned() {
+                //Event triggered when the pad is signed
+
+            }
+
+            override fun onClear() {
+                Toast.makeText(
+                    context,
+                    "Desenho apagado com sucesso!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+
+        })
+
+        binding.btnDelete.setOnClickListener {
+            binding.signaturePad.clear()
+        }
+
+
+        binding.btnChangeColor.setOnClickListener {
+            binding.colChangeColor.visibility = View.VISIBLE
+            binding.btnChangeConfirm.visibility = View.VISIBLE
+            binding.colChangeColor.setColorSelectionListener(object : SimpleColorSelectionListener() {
+                override fun onColorSelected(color: Int) {
+                    // Do whatever you want with the color
+                    binding.signaturePad.setPenColor(color)
+                }
+            })
+        }
+
+        binding.btnChangeConfirm.setOnClickListener {
+            it.visibility = View.GONE
+            binding.colChangeColor.visibility = View.GONE
+        }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment drawnFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            drawnFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
+
 }
